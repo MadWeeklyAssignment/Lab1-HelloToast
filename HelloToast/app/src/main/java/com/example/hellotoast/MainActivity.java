3df7f401a9860dc4ccb849b6bfa3e889303927b8
AnantaNeupane <neupaneananta18@gmail.com>
2021-03-18 13:22:51 +0545

    Add the onClick attribute and handler to each Button

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showToast(View view) {
+    }
+
+    public void countUp(View view) {
+    }
 }
